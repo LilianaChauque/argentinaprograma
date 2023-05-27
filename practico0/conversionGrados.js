80a2a49d339e9c68683176d28c3742363a8259f5
Liliana Chauque <lilianachauque27@hotmail.com>
2023-05-27 16:09:56 -0300

    Agrego el archivo conversionGrados.js

--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -7,8 +7,11 @@ let Celsius = (Fahrenheit - 32) * 5/9;
 Fahrenheit = 50;
 Celsius = (Fahrenheit - 32) * 5/9;
 console.log (Celsius);
+// Rta: 10
+
 
 // b.
 Fahrenheit = 70;
 Celsius = (Fahrenheit - 32) * 5/9;
 console.log (Celsius);
+// Rta: 21.11
