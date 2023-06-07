8c3f7428443e3b6b207140f9e6fa2b161cf0c173
Liliana Chauque <lilianachauque27@hotmail.com>
2023-06-06 21:38:55 -0300

    empezando práctico 1

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,14 @@
+//inciso a
+let nombre;
+
+//imciso b
+nombre = "Liliana";
+
+// inciso c
+let apellido = "Chauque";
+
+// inciso d 
+let edad = 47;
+
+// inciso e
+console.log("Hola, "+nombre+ " " +apellido+ ". Tienes "+edad+ " años.");
