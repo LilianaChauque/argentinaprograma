efb08c1646dbae03a46a036ed2ebe4b687422c0a
Liliana Chauque <lilianachauque27@hotmail.com>
2023-06-10 11:10:30 -0300

    agrego

--- practico1/frutas.js
+++ practico1/frutas.js
@@ -2,12 +2,12 @@
 let frutas = [];
 
 // inciso b
- frutas = ["anana", "cereza", "frutilla", "manzana", "palta"];
+ frutas.push("anana", "cereza", "frutilla", "manzana", "palta");
 
 // inciso c
 let ultima = frutas[frutas.length - 1];
-console.log("La ultima fruta agregada fue: " ,ultima);
+console.log("La ultima fruta agregada fue: ",ultima);
 
 // inciso d
 let primera = frutas[0];
-console.log("La primera futa agregada fue: " ,primera);
+console.log("La primera futa agregada fue: ",primera);
