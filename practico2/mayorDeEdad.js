06836e391c0daafc3b44ed60dd420c7ba5e79c9d
Liliana Chauque <lilianachauque27@hotmail.com>
2023-06-19 02:52:45 -0300

    subo ej. 1-a

---
+++ practico2/mayorDeEdad.js
@@ -0,0 +1,11 @@
+// Ejercicio 1, inc. a
+const readlineSync = require('readline-sync');
+
+let edad= readlineSync.question("Cual es tu edad ");
+ 
+if (edad >= 18){
+    console.log("Eres mayor de edad");  
+}
+else{
+    console.log("Eres menor de edad");
+}
